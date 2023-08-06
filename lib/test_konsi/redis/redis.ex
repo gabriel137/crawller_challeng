e8e44118a01092b169d25bdc0fa117e1dc90469f
gabriel137 <gabriel7@ucl.br>
2023-08-06 14:03:11 -0300

    redix don'nt keeping conection

--- lib/test_konsi/redis/redis.ex
+++ lib/test_konsi/redis/redis.ex
@@ -12,85 +12,85 @@ defmodule TestKonsi.Redis do
     end
   end
 
-  # def cache_result(cpf, result) do
-  #   with :ok <- Redix.command(:redix, ["SET", "cpf#{cpf}", result]) do
-  #     # Resultado cacheado com sucesso
-  #     :ok
-  #   else
-  #     _ ->
-  #       # Trate o erro, se necessário
-  #       :error
-  #   end
-  # end
+  def cache_result(cpf, result) do
+    with :ok <- Redix.command(:redix, ["SET", "cpf#{cpf}", result]) do
+      # Resultado cacheado com sucesso
+      :ok
+    else
+      _ ->
+        # Trate o erro, se necessário
+        :error
+    end
+  end
 
-  # def store_data(data) do
-  #   case start_link() do
-  #     {:ok, redix} ->
-  #       key = generate_redis_key()
-  #       Redix.command(redix, ["SET", key, Poison.encode!(data)])
+  def store_data(data) do
+    case start_link() do
+      {:ok, redix} ->
+        key = generate_redis_key()
+        Redix.command(redix, ["SET", key, Poison.encode!(data)])
 
-  #       cpf = data["cpf"]
-  #       pid = self()|> to_string()
-  #       Redix.command(redix, ["HSET", "cpf_to_pid", cpf, pid])
+        cpf = data["cpf"]
+        pid = self()|> to_string()
+        Redix.command(redix, ["HSET", "cpf_to_pid", cpf, pid])
 
-  #       # Feche a conexão com o Redis
-  #       Redix.stop(redix)
+        # Feche a conexão com o Redis
+        Redix.stop(redix)
 
-  #       {pid, key}
-  #     _ ->
-  #       IO.puts("Erro ao conectar ao Redis.")
-  #       {nil, nil}
-  #   end
-  # end
+        {pid, key}
+      _ ->
+        IO.puts("Erro ao conectar ao Redis.")
+        {nil, nil}
+    end
+  end
 
-  # defp generate_redis_key() do
-  #   "data_#{Base.encode16(:erlang.unique_integer([:positive, :monotonic]))}"
-  # end
+  defp generate_redis_key() do
+    "data_#{Base.encode16(:erlang.unique_integer([:positive, :monotonic]))}"
+  end
 
-  # def get_data_from_redis(pid) do
-  #   case start_link() do
-  #     {:ok, redix} ->
-  #       key = "data_#{pid}"
-  #       case Redix.command(redix, ~w(GET) [key]) do
-  #         {:ok, data_json} ->
-  #           # Deserialize os dados do JSON
-  #           {:ok, data} = Poison.decode(data_json)
-  #           Redix.stop(redix)
-  #           {:ok, data}
+  def get_data_from_redis(pid) do
+    case start_link() do
+      {:ok, redix} ->
+        key = "data_#{pid}"
+        case Redix.command(redix, ~w(GET) [key]) do
+          {:ok, data_json} ->
+            # Deserialize os dados do JSON
+            {:ok, data} = Poison.decode(data_json)
+            Redix.stop(redix)
+            {:ok, data}
 
-  #         :nil ->
-  #           Redix.stop(redix)
-  #           {:error, "Dados não encontrados"}
+          :nil ->
+            Redix.stop(redix)
+            {:error, "Dados não encontrados"}
 
-  #         _ ->
-  #           Redix.stop(redix)
-  #           {:error, "Erro ao buscar dados no Redis"}
-  #       end
-  #     _ ->
-  #       {:error, "Erro ao conectar ao Redis"}
-  #   end
-  # end
+          _ ->
+            Redix.stop(redix)
+            {:error, "Erro ao buscar dados no Redis"}
+        end
+      _ ->
+        {:error, "Erro ao conectar ao Redis"}
+    end
+  end
 
-  # defp get_pid_from_redis(cpf) do
-  #   redis_url = Application.get_env(:test_konsi, Crawler)[:redis_url]
+  defp get_pid_from_redis(cpf) do
+    redis_url = Application.get_env(:test_konsi, Crawler)[:redis_url]
 
-  #   case Redix.start_link(redis_url) do
-  #     {:ok, redix} ->
-  #       case Redix.command(redix, ~w(HGET), ["cpf_to_pid", cpf]) do
-  #         {:ok, pid} ->
-  #           Redix.stop(redix)
-  #           {:ok, pid}
+    case Redix.start_link(redis_url) do
+      {:ok, redix} ->
+        case Redix.command(redix, ~w(HGET), ["cpf_to_pid", cpf]) do
+          {:ok, pid} ->
+            Redix.stop(redix)
+            {:ok, pid}
 
-  #         :nil ->
-  #           Redix.stop(redix)
-  #           {:error, "CPF não encontrado"}
+          :nil ->
+            Redix.stop(redix)
+            {:error, "CPF não encontrado"}
 
-  #         _ ->
-  #           Redix.stop(redix)
-  #           {:error, "Erro ao buscar o CPF no Redis"}
-  #       end
-  #     _ ->
-  #       {:error, "Erro ao conectar ao Redis"}
-  #   end
-  # end
+          _ ->
+            Redix.stop(redix)
+            {:error, "Erro ao buscar o CPF no Redis"}
+        end
+      _ ->
+        {:error, "Erro ao conectar ao Redis"}
+    end
+  end
 end
